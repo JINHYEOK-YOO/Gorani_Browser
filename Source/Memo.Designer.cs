10ad7e0626a08e397a45db8812752a6004f2e239
Yoo Jinhyeok <yujh703@gmail.com>
2020-05-15 06:50:58 +0900

    Upload

---
+++ Source/Memo.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace GoraniBrowser
+{
+    partial class frmMemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trbThick = new System.Windows.Forms.TrackBar();
+            this.pnlShowColor = new System.Windows.Forms.Panel();
+            this.pnlIcon = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNote = new System.Windows.Forms.Button();
+            this.btnEraser = new System.Windows.Forms.Button();
+            this.btnDrawing = new System.Windows.Forms.Button();
+            this.pnlBoard = new System.Windows.Forms.Panel();
+            this.picMemo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trbThick)).BeginInit();
+            this.pnlIcon.SuspendLayout();
+            this.pnlBoard.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMemo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trbThick
+            // 
+            this.trbThick.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.trbThick.Cursor = System.Windows.Forms.Cursors.Default;
+            this.trbThick.LargeChange = 1;
+            this.trbThick.Location = new System.Drawing.Point(540, 33);
+            this.trbThick.Margin = new System.Windows.Forms.Padding(0);
+            this.trbThick.Minimum = 1;
+            this.trbThick.Name = "trbThick";
+            this.trbThick.Size = new System.Drawing.Size(76, 56);
+            this.trbThick.TabIndex = 7;
+            this.trbThick.TickFrequency = 10;
+            this.trbThick.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trbThick.Value = 1;
+            this.trbThick.Scroll += new System.EventHandler(this.trbThick_Scroll);
+            // 
+            // pnlShowColor
+            // 
+            this.pnlShowColor.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pnlShowColor.BackColor = System.Drawing.Color.Black;
+            this.pnlShowColor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pnlShowColor.Location = new System.Drawing.Point(550, 4);
+            this.pnlShowColor.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlShowColor.Name = "pnlShowColor";
+            this.pnlShowColor.Size = new System.Drawing.Size(60, 25);
+            this.pnlShowColor.TabIndex = 8;
+            this.pnlShowColor.Click += new System.EventHandler(this.pnlShowColor_Click);
+            // 
+            // pnlIcon
+            // 
+            this.pnlIcon.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlIcon.Controls.Add(this.btnSave);
+            this.pnlIcon.Controls.Add(this.btnNote);
+            this.pnlIcon.Controls.Add(this.btnEraser);
+            this.pnlIcon.Controls.Add(this.btnDrawing);
+            this.pnlIcon.Controls.Add(this.pnlShowColor);
+            this.pnlIcon.Controls.Add(this.trbThick);
+            this.pnlIcon.Location = new System.Drawing.Point(11, 5);
+            this.pnlIcon.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlIcon.Name = "pnlIcon";
+            this.pnlIcon.Size = new System.Drawing.Size(1160, 60);
+            this.pnlIcon.TabIndex = 10;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnSave.BackgroundImage = global::GoraniBrowser.Properties.Resources.Save;
+            this.btnSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.Location = new System.Drawing.Point(700, 2);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(55, 55);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNote
+            // 
+            this.btnNote.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnNote.BackColor = System.Drawing.SystemColors.Control;
+            this.btnNote.BackgroundImage = global::GoraniBrowser.Properties.Resources.note;
+            this.btnNote.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnNote.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNote.Location = new System.Drawing.Point(630, 2);
+            this.btnNote.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNote.Name = "btnNote";
+            this.btnNote.Size = new System.Drawing.Size(55, 55);
+            this.btnNote.TabIndex = 13;
+            this.btnNote.UseVisualStyleBackColor = false;
+            this.btnNote.Click += new System.EventHandler(this.btnNote_Click);
+            // 
+            // btnEraser
+            // 
+            this.btnEraser.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnEraser.BackColor = System.Drawing.SystemColors.Control;
+            this.btnEraser.BackgroundImage = global::GoraniBrowser.Properties.Resources.Rubber;
+            this.btnEraser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnEraser.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEraser.Location = new System.Drawing.Point(470, 2);
+            this.btnEraser.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEraser.Name = "btnEraser";
+            this.btnEraser.Size = new System.Drawing.Size(55, 55);
+            this.btnEraser.TabIndex = 11;
+            this.btnEraser.UseVisualStyleBackColor = false;
+            this.btnEraser.Click += new System.EventHandler(this.btnEraser_Click);
+            // 
+            // btnDrawing
+            // 
+            this.btnDrawing.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnDrawing.BackColor = System.Drawing.SystemColors.Control;
+            this.btnDrawing.BackgroundImage = global::GoraniBrowser.Properties.Resources.Pencil;
+            this.btnDrawing.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnDrawing.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDrawing.Location = new System.Drawing.Point(400, 2);
+            this.btnDrawing.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDrawing.Name = "btnDrawing";
+            this.btnDrawing.Size = new System.Drawing.Size(55, 55);
+            this.btnDrawing.TabIndex = 10;
+            this.btnDrawing.UseVisualStyleBackColor = false;
+            this.btnDrawing.Click += new System.EventHandler(this.btnDrawing_Click);
+            // 
+            // pnlBoard
+            // 
+            this.pnlBoard.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlBoard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlBoard.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlBoard.Controls.Add(this.picMemo);
+            this.pnlBoard.Location = new System.Drawing.Point(9, 69);
+            this.pnlBoard.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlBoard.Name = "pnlBoard";
+            this.pnlBoard.Size = new System.Drawing.Size(1162, 573);
+            this.pnlBoard.TabIndex = 11;
+            // 
+            // picMemo
+            // 
+            this.picMemo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picMemo.BackColor = System.Drawing.Color.Transparent;
+            this.picMemo.Location = new System.Drawing.Point(0, 0);
+            this.picMemo.Name = "picMemo";
+            this.picMemo.Size = new System.Drawing.Size(1160, 571);
+            this.picMemo.TabIndex = 0;
+            this.picMemo.TabStop = false;
+            this.picMemo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picMemo_MouseDown);
+            this.picMemo.MouseEnter += new System.EventHandler(this.picMemo_MouseEnter);
+            this.picMemo.MouseLeave += new System.EventHandler(this.pieMemo_MouseLeave);
+            this.picMemo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picMemo_MouseMove);
+            // 
+            // frmMemo
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1182, 653);
+            this.Controls.Add(this.pnlBoard);
+            this.Controls.Add(this.pnlIcon);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "frmMemo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gorani Memo";
+            ((System.ComponentModel.ISupportInitialize)(this.trbThick)).EndInit();
+            this.pnlIcon.ResumeLayout(false);
+            this.pnlIcon.PerformLayout();
+            this.pnlBoard.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picMemo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TrackBar trbThick;
+        private System.Windows.Forms.Panel pnlShowColor;
+        private System.Windows.Forms.Panel pnlIcon;
+        private System.Windows.Forms.Button btnDrawing;
+        private System.Windows.Forms.Button btnEraser;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNote;
+        private System.Windows.Forms.Panel pnlBoard;
+        private System.Windows.Forms.PictureBox picMemo;
+    }
+}
---
+++ Source/Memo.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoraniBrowser
+{
+    enum DRAW_MODE : int
+    {
+        PENMODE = 0,        // 펜 모드
+        NOTEMODE = 1,      // 노트 모드
+        EDITMODE = 2        // 그 외 편집 모드
+    }
+
+    public partial class frmMemo : Form
+    {
+        private int curMode;    // 현재 커서 모드
+        private Color curColor = Color.Black;   // 선 색깔
+        private int curLineSize = 1;    // 선 굵기
+        private Point p;    // 마우스 좌표 저장
+        private Bitmap pictureBoxBmp;   // 선 그려지는 이미지
+
+        public Image pnlBoardImage
+        {
+            get
+            {
+                return pnlBoard.BackgroundImage;
+            }
+            set
+            {
+                pnlBoard.BackgroundImage = value;
+            }
+        }
+
+        public frmMemo()
+        {
+            InitializeComponent();
+            pnlShowColor.BackColor = curColor;
+            SetDrawMode((int)DRAW_MODE.PENMODE);
+            pictureBoxBmp = new Bitmap(picMemo.Width, picMemo.Height);
+        }
+
+        // 그리기 모드 변경
+        private void SetDrawMode(int mode)
+        {
+            switch (mode)
+            {
+                case (int)DRAW_MODE.PENMODE:    // 펜 모드
+                    curMode = (int)DRAW_MODE.PENMODE;
+                    MemoryStream cursorMemoryStream = new MemoryStream(Properties.Resources.PenCursor_small);
+                    Cursor = new Cursor(cursorMemoryStream);
+                    break;
+                case (int)DRAW_MODE.NOTEMODE:   // 노트 모드
+                    curMode = (int)DRAW_MODE.NOTEMODE;
+                    Cursor = Cursors.Hand;
+                    break;
+                case (int)DRAW_MODE.EDITMODE:   // 그외
+                    Cursor = Cursors.Default;
+                    break;
+                default:
+                    Cursor = Cursors.Default;
+                    break;
+            }
+        }
+
+        // 펜 버튼 클릭 시 펜 모드로 변경
+        private void btnDrawing_Click(object sender, EventArgs e) => SetDrawMode((int)DRAW_MODE.PENMODE);
+
+        // 지우개 버튼 클릭 시 이미지 지우고 새로 그림
+        private void btnEraser_Click(object sender, EventArgs e)
+        {
+            picMemo.Dispose();
+            picMemo = new PictureBox();
+            picMemo.BackColor = Color.Transparent;
+            picMemo.Dock = DockStyle.Fill;
+            picMemo.Location = new Point(0, 0);
+            picMemo.Name = "picMemo";
+            picMemo.Size = new Size(1160, 571);
+            picMemo.TabIndex = 0;
+            picMemo.TabStop = false;
+            picMemo.MouseDown += new MouseEventHandler(picMemo_MouseDown);
+            picMemo.MouseEnter += new EventHandler(picMemo_MouseEnter);
+            picMemo.MouseLeave += new EventHandler(pieMemo_MouseLeave);
+            picMemo.MouseMove += new MouseEventHandler(picMemo_MouseMove);
+            pictureBoxBmp = new Bitmap(picMemo.Width, picMemo.Height);
+            pnlBoard.Controls.Add(picMemo);
+        }
+
+        // 색 선택 창 띄우기
+        private void pnlShowColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                curColor = colorDialog.Color;
+                pnlShowColor.BackColor = curColor;
+            }
+        }
+
+        // 노트 버튼 선택 시 노트 모드로 변경
+        private void btnNote_Click(object sender, EventArgs e) => SetDrawMode((int)DRAW_MODE.NOTEMODE);
+
+        // 이미지로 저장하기
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = ".png";
+            saveFileDialog.Filter = "PNG File|*.png|Bitmap File|*.bmp|JPEG File|*.jpg";
+            saveFileDialog.OverwritePrompt = true;
+
+            Bitmap img = new Bitmap(picMemo.Width, picMemo.Height);
+            Rectangle rect = new Rectangle(picMemo.Location, picMemo.Size);
+            pnlBoard.DrawToBitmap(img, rect);
+
+            if (img == null)
+            {
+                MessageBox.Show("이미지가 없습니다!");
+                return;
+            }
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                switch (saveFileDialog.FilterIndex)
+                {
+                    case 1:
+                        img.Save(saveFileDialog.FileName, ImageFormat.Png);
+                        break;
+
+                    case 2:
+                        img.Save(saveFileDialog.FileName, ImageFormat.Bmp);
+                        break;
+
+                    case 3:
+                        img.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                        break;
+                }
+            }
+        }
+
+        // 메모 패널에서 마우스 클릭 시
+        private void picMemo_MouseDown(object sender, MouseEventArgs e)
+        {
+            // 펜 모드면 좌표 저장
+            if (curMode == (int)DRAW_MODE.PENMODE && e.Button == MouseButtons.Left)
+            {
+                p.X = e.X;
+                p.Y = e.Y;
+            }
+            // 노트 모드면 노트 생성
+            else if (curMode == (int)DRAW_MODE.NOTEMODE && e.Button == MouseButtons.Left)
+            {
+                TextBox t = new TextBox();
+                t.Location = new Point(e.X, e.Y);
+                t.Multiline = true;
+                t.Size = new Size(100, 160);
+                t.BackColor = Color.Gold;
+                t.Font = new Font("맑은 고딕", 10, FontStyle.Bold);
+                t.ImeMode = ImeMode.NoControl;
+                picMemo.Controls.Add(t);
+            }
+        }
+
+        // 메모 패널에서 마우스 이동 시
+        private void picMemo_MouseMove(object sender, MouseEventArgs e)
+        {
+            // 펜 모드면 선 그리기
+            if (curMode == (int)DRAW_MODE.PENMODE && e.Button == MouseButtons.Left)
+            {
+                Pen pen = new Pen(curColor, curLineSize);
+
+                Graphics g = Graphics.FromImage(pictureBoxBmp);
+                g.DrawLine(pen, p.X, p.Y, e.X, e.Y);
+                p.X = e.X;
+                p.Y = e.Y;
+                picMemo.Image = pictureBoxBmp;
+                g.Dispose();
+            }
+        }
+
+        // 메모 패널에 마우스 들어갈 경우 /  나올 경우
+        private void picMemo_MouseEnter(object sender, EventArgs e) => SetDrawMode(curMode);
+        private void pieMemo_MouseLeave(object sender, EventArgs e) => SetDrawMode((int)DRAW_MODE.EDITMODE);
+
+        // 선 굵기 조절 스크롤
+        private void trbThick_Scroll(object sender, EventArgs e) => curLineSize = trbThick.Value;
+    }
+}
